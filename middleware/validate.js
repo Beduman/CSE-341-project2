2bed34dacbb9a2f22f6bb2cb65e7c93fcc04f92c
Beduman <behunz77@gmail.com>
2025-06-07 16:36:45 -0700

    is a certain key count

--- middleware/validate.js
+++ middleware/validate.js
@@ -4,9 +4,9 @@ const userValidationRules = () => {
     body('name').isLength({max:30}),
     body('contact').isEmail(),
     body('employees').isLength(5),
-    body('type').isString({ max: 20 }),
-    body('model').isString({ max: 30 }),
-    body('serialNumber').isString({ max: 50 }),
+    body('type').isLength({ max: 20 }),
+    body('model').isLength({ max: 30 }),
+    body('serialNumber').isLength({ max: 50 }),
     body('cpu').isLength({ max: 30 }),
     body('ram').isLength({ min: 10 }),
   ]
